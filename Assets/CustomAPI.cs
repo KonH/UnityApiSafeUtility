679df5cb76ff6ad81a87863b10b353740882257e
KonH <konh@yandex.ru>
2017-03-16 22:32:22 +0700

    Extended example and detailed logs;

--- Assets/CustomAPI.cs
+++ Assets/CustomAPI.cs
@@ -1,2 +1,46 @@
 ﻿public class CustomAPI {
+
+	public static int StaticField = 42;
+
+	public int MemberField = 91;
+
+	int _privateField;
+
+	public static int StaticIntMemberProperty {
+		get; private set;
+	}
+
+	public int IntMemberProperty {
+		get; private set;
+	}
+
+	public string StringMemberProperty {
+		get; set;
+	}
+
+	public CustomAPI() {
+	}
+
+	public CustomAPI(string arg) {
+	}
+
+	public static void VoidStaticMethod() {
+	}
+
+	public static int IntReturningStaticMethod() {
+		return 0;
+	}
+
+	static void PrivateStaticMethod() {
+	}
+
+	public void VoidMemberMethod() {
+	}
+
+	public string StringMemberMethod() {
+		return string.Empty;
+	}
+
+	void PrivateMemberMethod() {
+	}
 }
